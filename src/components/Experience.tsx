7ff22be8cfa4bcd52f9b2eb82fc3baf6630f8375
Zahra Khodabakhshian <zahrakhodabakhshian54@gmail.com>
2024-05-15 10:16:27 +0200

    Add animation to Education Section

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,21 +1,79 @@
-import { Grid } from "@mui/material";
+import { Grid, Grow } from "@mui/material";
+import { useCallback, useRef, useState } from "react";
 import Item from "./Item";
 import educationorg from "/home/zaranix/portfolio/src/assets/Images/eduOrange.svg";
 import experience from "/home/zaranix/portfolio/src/assets/Images/exp.svg";
+
 interface Theme {
   palette: {
     background: { default: string; paper: string };
     text: { primary: string; secondary: string };
   };
 }
+
 interface Props {
   theme: Theme;
 }
 
 const Experience = ({ theme }: Props) => {
+  const [visible, setVisible] = useState(false);
+
+  const observerRef = useRef<IntersectionObserver | null>(null);
+
+  const setRef = useCallback((node: HTMLDivElement | null) => {
+    if (observerRef.current) {
+      observerRef.current.disconnect();
+    }
+
+    observerRef.current = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setVisible(true);
+          } else {
+            setVisible(false);
+          }
+        });
+      },
+      { threshold: 0.1 }
+    );
+
+    if (node) {
+      observerRef.current.observe(node);
+    }
+  }, []);
+
+  const items = [
+    {
+      date: "Jul 2016 - Apr 2020",
+      title: "Insurance agent",
+      school: "Pasargad Insurance Agency",
+      imageUrl: experience,
+    },
+    {
+      date: "Oct 2020 - Oct 2021",
+      title: "Real estate agent",
+      school: "Dolati Real Estate Agency",
+      imageUrl: experience,
+    },
+    {
+      date: "Jun 2021 - Dec 2021",
+      title: "Front-end Developer (Internship)",
+      school: "Shahre Wordpress",
+      imageUrl: experience,
+    },
+    {
+      date: "Jul 2022 - Jul 2023",
+      title: "Front-end Developer",
+      school: "Kian Development Holding (Abzar Wordpress)",
+      imageUrl: experience,
+    },
+  ];
+
   return (
     <Grid
       id="experience"
+      ref={setRef}
       sx={{
         backgroundColor: theme.palette.background.paper,
         height: "100vh",
@@ -34,30 +92,25 @@ const Experience = ({ theme }: Props) => {
         >
           Experience
         </h1>
-        <Item
-          date="Jul 2016 - Apr 2020"
-          title="Insurance agent"
-          school="Pasargad Insurance Agency"
-          imageUrl={experience}
-        />
-        <Item
-          date="Oct 2020 - Oct 2021"
-          title="Real estate agent"
-          school="Dolati Real Estate Agency"
-          imageUrl={experience}
-        />
-        <Item
-          date="Jun 2021 - Dec 2021"
-          title="Front-end Developer (Internship)"
-          school="Shahre Wordpress"
-          imageUrl={experience}
-        />
-        <Item
-          date="Jul 2022 - Jul 2023"
-          title="Front-end Developer"
-          school="Kian Development Holding (Abzar Wordpress)"
-          imageUrl={experience}
-        />
+
+        {items.map((item, index) => (
+          <Grow
+            key={index}
+            in={visible}
+            timeout={{
+              enter: 1000 + index * 500, // Delay each item by 500ms after the previous one
+            }}
+          >
+            <div>
+              <Item
+                date={item.date}
+                title={item.title}
+                school={item.school}
+                imageUrl={item.imageUrl}
+              />
+            </div>
+          </Grow>
+        ))}
       </Grid>
       <Grid
         style={{ display: "flex", justifyContent: "center" }}
